bf27e551abe0ec60c9b424f9e1d0cdfaf646c69a
Kevin Kiningham <kevinnk@umich.edu>
2011-06-23 13:09:59 -0400

    added a main function, moved parser to own folder

---
+++ c/common.h
@@ -0,0 +1,3 @@
+enum ERROR {
+	ERROR_OK = 0
+};
---
+++ c/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "common.h"
+
+extern FILE* yyin;
+extern int yyparse(void);
+
+int main( int argc, char** argv ) {
+
+	
+
+	return ERROR_OK;
+}
